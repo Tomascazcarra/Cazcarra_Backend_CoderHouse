3e8d3d5612961f93187b59246fddebfa9a8237dc
Tomas Cazcarra <tomascazcarra@gmail.com>
2023-09-04 23:11:17 -0300

    entrega tp

--- test/routes/carts/supertest-routes-carts.test.js
+++
@@ -1,7 +0,0 @@
-import chai from "chai";
-import supertest from "supertest";
-
-const expect = chai.expect;
-const request = supertest("http://localhost:8080")
-
-//REPETIR LO HECHO EN PRODUCTS
--- test/routes/products/supertest-routes-products.test.js
+++ test/routes/products/supertest-routes-products.test.js
@@ -5,14 +5,9 @@ import config from "../../../src/config/config.js";
 const expect = chai.expect;
 const request = supertest("http://localhost:8080")
 
-describe("testing integrador de products",()=>{
-    describe("test de productos",()=>{
-        it("endpoint GET /api/products debera responder status+payload", async function(){   
-            const {_body} = await request.get("/api/products/")
-            expect (_body).to.have.property("status")
-            expect (_body).to.have.property("payload").and.to.be.an("array")
-        })
-        //Registro con cookie
+describe("Supertest de routes",()=>{
+
+        //TEST DE SESSIONS
         let cookie;
         it("el endpoint POST /register debe registrar correctamente un usuario", async function(){
             const mockUser = {
@@ -42,8 +37,14 @@ describe("testing integrador de products",()=>{
         it("el endpoint GET / debe recibir la cookie del user y devolverlo",async function(){
             const response = await request.get("/").set("Cookie",[`${cookie.name}=${cookie.value}`])
         })
-        
-        
+
+        //TEST DE PRODUCTS
+
+        it("endpoint GET /api/products debera responder status+payload", async function(){   
+            const {_body} = await request.get("/api/products/")
+            expect (_body).to.have.property("status")
+            expect (_body).to.have.property("payload").and.to.be.an("array")
+        })
         it("endpoint POST /api/products debera crear correctamente un producto", async function(){   
             const mockProduct = {
                 title: "prueba1s3123",
@@ -57,26 +58,29 @@ describe("testing integrador de products",()=>{
             expect(status).to.be.equals(201)
 
         })
-        it("endpoint POST /api/products NO debera crear correctamente un producto debido a falta de datos", async function(){
-            const invalidProduct = {
-                title: "prueba1"
-            }
-            try{
-                const response = await request.post("/api/products/").set("Cookie", [`${cookie.name}=${cookie.value}`]).send(invalidProduct)
-            }catch(error) {
-                console.log(error.response)
-                expect(error.status).to.be.equals(400)
-            }
-            
-            /*
-            const response = await request.post("/api/products/").set("Cookie", [`${cookie.name}=${cookie.value}`]).send(invalidProduct)
-            const {status,_body} = response;
-            expect(status).to.be.eql(400)*/
+
+
+        //TEST DE CARTS
+        it("endpoint GET /api/carts debera responder status+payload", async function(){   
+            const {_body} = await request.get("/api/carts/")
+            expect (_body).to.have.property("status")
+            expect (_body).to.have.property("payload").and.to.be.an("array")
         })
+
+        it("endpoint POST /api/carts debera crear correctamente un carrito", async function(){   
+            const mockCart = {
+                product:"prueba",
+                quantity:12
+            }
+            const response = await request.post("/api/carts/").set("Cookie", [`${cookie.name}=${cookie.value}`]).send(mockCart)
+            const {status,_body} = response;
+            expect(status).to.be.equals(201)
+        })
+
     })
 
     
-})
+
 
 
 
